aaa9b72da491d9bf7ed73bf4cb1ec92c16044ea4
Abi Broad <abibroad@gmail.com>
2023-02-24 18:51:39 +0000

    Shared code - poker generate a hand and checking functions

--- poker_probability.js
+++ poker_probability.js
@@ -1,82 +1,125 @@
-const NUM_TRIALS = 0
-
-//write the function drawFiveCards();
-
-function drawFiveCards() {
-    //draw 5 cards
-    //iterate 5 times
-    let hand = []; //empty array for the cards
-    //loop to draw cards 5 times
-    for (let i = 0; i < 5; i++) {
-        //draw a card, put it in the return hand.
-        let card = drawOneCard();
-    //checking if card is already in the array/hand
-        while (hand.includes(card)) {
-            card = drawOneCard();
-        }
-        hand.push(card); //push card to array
-    }
-    return hand; //after loop but before end of the function return the hand
-}
-
-//return a card to the caller
+const NUM_TRIALS = 0;
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const numbers = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6', '5', '4', '3', '2']
 function drawOneCard() {
-    const suits = "SHDC";
-    const numbers = "23456789TQKA";
-    let suit = suits[Math.floor(Math.random() * suits.length)];
-    let number = numbers[Math.floor(Math.random() * numbers.length)];
-    return suit + number;
+    let oneSuit = Math.floor(Math.random() * suits.length);
+    let oneNumber = Math.floor(Math.random() * numbers.length);
+    let DrawnCard = numbers[oneNumber] + " " + suits[oneSuit]
+    return DrawnCard
 }
-
-//main logic:
-let hand = drawFiveCards().join(' ');
-console.log(hand);
-
-//a hand like HA HT C4 D3 C8 will be generated
-//pulling out the numbers
-function numbers() {
-    check = hand.split(" "); //array
+//Draw 5 cards
+function drawFiveCards() {
+    let cards = []
     for (let i = 0; i < 5; i++) {
-        check[i] = check[i].substr(1);
-    } return check 
+        let card = drawOneCard()
+        while (cards.includes(card)) {
+            card = drawOneCard()
+        }
+        cards.push(card)
+    }
+    return cards
 }
-
+//drawing 5cards
+const hand = drawFiveCards()
+let draw = hand.join(' ')
+//get suitOrder
+function getSuitOrder() {
+    let arrSuits = []
+    let value = hand.join(' ').split(' ')
+    for (let i = 0; i < 10; i += 2) {
+        let drawnSuits = value[i]
+        arrSuits.push(drawnSuits)
+    }
+    return arrSuits;
+}
+let getnumberorder = getSuitOrder()
+//get order
+function getOrder() {
+    let arrOrder = []
+    let value = hand.join(' ').split(' ')
+    for (let i = 1; i < 10; i += 2) {
+        let drawnSuits = value[i]
+        arrOrder.push(drawnSuits)
+    }
+    return arrOrder;
+}
+let getsuitorder = getOrder()
+//compare arrays to confirm sequence
+function test(b, a, bIndex) {
+    if (!a.length) return true;
+    bIndex ??= b.findIndex(el => el === a[0]);
+    return a[0] !== b[bIndex] ? false : test(b.slice(bIndex + 1), a.slice(1), 0);
+}
+function checkTest() {
+    let sOrder = []
+    sOrder.push(...getnumberorder)
+    return test(numbers, sOrder)
+}
+//check type of hand drawn
+//flush
+function flush(hand) {
+    for (let item of suits) {
+        if (hand.split(item).length - 1 === 5 && checkTest() === false) {
+            return true
+        }
+    }
+}
+//straight flush
+function straightflush(hand) {
+    for (let item of suits) {
+        if (hand.split(item).length - 1 === 5 && checkTest() === true) {
+            return true
+        }
+    }
+}
+//straight
+function straight(hand) {
+    for (let item of suits) {
+        if (hand.split(item).length - 1 === 2 && checkTest() === true) {
+            return true
+        }
+    }
+}
 //3 of a kind -- 3 cards with the same number
 function threeOfAKind() {
-    for (let item of numbers()) {
-        if (hand.split(item).length - 1 == 3) {
-            return true;
-        }
-    } 
-}
-
-//4 of a kind -- 4 cards with the same number
-function fourOfAKind() {
-    for (let item of numbers()) {
-        if (hand.split(item).length - 1 == 4) {
-            return true;
-        }
-    } 
-}
-
-//2 cards with the same number -- combined with 3 of a kind for full house in main logic
-function pairNumbers() {
-    for (let item of numbers()) {
-        if (hand.split(item).length - 1 == 2) {
+    for (let item of getnumberorder) {
+        if (draw.split(item).length - 1 == 3) {
             return true;
         }
     }
 }
-
+//4 of a kind -- 4 cards with the same number
+function fourOfAKind() {
+    for (let item of getnumberorder) {
+        if (draw.split(item).length - 1 == 4) {
+            return true;
+        }
+    }
+}
+//2 cards with the same number -- combined with 3 of a kind for full house in main logic
+function pairNumbers() {
+    for (let item of getnumberorder) {
+        if (draw.split(item).length - 1 == 2) {
+            return true;
+        }
+    }
+}
 // main logic
 //add straight flush, straight and flush
 //change console log to counts
-if (threeOfAKind(hand) && pairNumbers(hand)) {
-    console.log(hand + " is a full house");
+if (threeOfAKind(draw) && pairNumbers(draw)) {
+    console.log(draw + "  is a full house");
 }
-else if (fourOfAKind(hand)) {
-    console.log(hand + " is 4 of a kind");
+else if (fourOfAKind(draw)) {
+    console.log(draw + "  is 4 of a kind");
 }
-else if (threeOfAKind(hand)) {
-    console.log(hand + " is 3 of a kind");
+else if (threeOfAKind(draw)) {
+    console.log(draw + "  is 3 of a kind");
+}else if (flush(draw)) {
+    console.log(draw + "  is a flush")
+}else if (straightflush(draw)) {
+    console.log(draw + "  is a straight flush")
+}else if (straight(draw)) {
+    console.log(draw + "  is a straight")
 }
+console.log(draw)
